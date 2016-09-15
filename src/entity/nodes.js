2ba7ae00f9cc8e600ce65349572cfb753d79ba34
Shadows666 <lifeloveregretsxe@gmail.com>
2016-09-15 16:44:52 +0100

    set up treatment, symptom and causal pages to hide appropriate information when type is selected

--- src/entity/nodes.js
+++ src/entity/nodes.js
@@ -2,14 +2,22 @@ module.exports = (nga, admin) => {
   const node = nga.entity('nodes');
 
   const fields = [
-    nga.field('name'),
+    nga.field('name')
+    .cssClasses('name_class'),
     nga.field('type', 'choice')
+      .cssClasses('type_class')
       .choices([
         { value: 'symptom', label: 'Symptom' },
         { value: 'causal', label: 'Causal Factor' },
         { value: 'treatment', label: 'Treatments' },
       ]),
     nga.field('size', 'choice')
+      .cssClasses('intensity_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'treatment')
+        ) && 'hidden'
+      )
       .label('Intensity')
       .choices([
         { value: '1', label: '1' },
@@ -19,6 +27,12 @@ module.exports = (nga, admin) => {
         { value: '3', label: '5' },
       ]),
     nga.field('frequency', 'choice')
+      .cssClasses('frequency_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'treatment')
+        ) && 'hidden'
+      )
       .label('Frequency')
       .choices([
         { value: '1', label: '1' },
@@ -28,6 +42,12 @@ module.exports = (nga, admin) => {
         { value: '3', label: '5' },
       ]),
     nga.field('duration', 'choice')
+      .cssClasses('duration_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'treatment')
+        ) && 'hidden'
+      )
       .label('Duration')
       .choices([
         { value: '1', label: '1' },
@@ -37,6 +57,12 @@ module.exports = (nga, admin) => {
         { value: '3', label: '5' },
       ]),
     nga.field('modifiability', 'choice')
+      .cssClasses('modifiability_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'treatment')
+        ) && 'hidden'
+      )
       .label('Modifiability')
       .choices([
         { value: '1', label: '1' },
@@ -46,6 +72,12 @@ module.exports = (nga, admin) => {
         { value: '3', label: '5' },
       ]),
     nga.field('clientAgreement', 'choice')
+      .cssClasses('clientAgreement_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'treatment')
+        ) && 'hidden'
+      )
       .label('Client Agreement')
       .choices([
         { value: '1', label: '1' },
@@ -55,6 +87,12 @@ module.exports = (nga, admin) => {
         { value: '3', label: '5' },
       ]),
     nga.field('clinicianAgreement', 'choice')
+      .cssClasses('clinicianAgreement_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'symptom' || entry.values.type === 'causal')
+        ) && 'hidden'
+      )
       .label('Clinician Agreement')
       .choices([
         { value: '1', label: '1' },
@@ -64,6 +102,12 @@ module.exports = (nga, admin) => {
         { value: '3', label: '5' },
       ]),
     nga.field('MDTAgreement', 'choice')
+      .cssClasses('MDTAgreement_class')
+      .cssClasses(entry =>
+         (entry && entry.values &&
+          (entry.values.type === 'symptom' || entry.values.type === 'causal')
+        ) && 'hidden'
+      )
       .label('MDT Agreement')
       .choices([
         { value: '1', label: '1' },
@@ -72,11 +116,13 @@ module.exports = (nga, admin) => {
         { value: '2.5', label: '4' },
         { value: '3', label: '5' },
       ]),
-    nga.field('description'),
+    nga.field('description', 'text')
+      .cssClasses('description_class'),
     nga.field('clientId')
       .cssClasses('hidden')
       .label(''),
   ];
+
 
   node
     .listView()
