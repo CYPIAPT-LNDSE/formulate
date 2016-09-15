cd119612f92e8e907c7a05dc3ee2fe112fc8c348
mcelearr <rorymcelearney@gmail.com>
2016-09-15 13:10:35 +0100

    adds new variables to the forms

--- src/entity/edges.js
+++ src/entity/edges.js
@@ -10,6 +10,15 @@ module.exports = (nga, admin) => {
       .targetEntity(nga.entity('nodes'))
       .targetField(nga.field('name'))
       .validation({ required: true }),
+    nga.field('strengthOfRelationship', 'choice')
+      .label('Strength of Relationship')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
     nga.field('clientId')
       .cssClasses('hidden')
       .label(''),
--- src/entity/nodes.js
+++ src/entity/nodes.js
@@ -10,7 +10,7 @@ module.exports = (nga, admin) => {
         { value: 'treatment', label: 'Treatments' },
       ]),
     nga.field('size', 'choice')
-      .label('Strength')
+      .label('Intensity')
       .choices([
         { value: '1', label: '1' },
         { value: '1.5', label: '2' },
@@ -18,6 +18,61 @@ module.exports = (nga, admin) => {
         { value: '2.5', label: '4' },
         { value: '3', label: '5' },
       ]),
+    nga.field('frequency', 'choice')
+      .label('Frequency')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
+    nga.field('duration', 'choice')
+      .label('Duration')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
+    nga.field('modifiability', 'choice')
+      .label('Modifiability')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
+    nga.field('clientAgreement', 'choice')
+      .label('Client Agreement')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
+    nga.field('clinicianAgreement', 'choice')
+      .label('Clinician Agreement')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
+    nga.field('MDTAgreement', 'choice')
+      .label('MDT Agreement')
+      .choices([
+        { value: '1', label: '1' },
+        { value: '1.5', label: '2' },
+        { value: '2', label: '3' },
+        { value: '2.5', label: '4' },
+        { value: '3', label: '5' },
+      ]),
+    nga.field('description'),
     nga.field('clientId')
       .cssClasses('hidden')
       .label(''),
