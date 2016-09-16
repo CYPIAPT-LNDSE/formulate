7d39306834ad74514d45b9e20d56671ff1096d40
mcelearr <rorymcelearney@gmail.com>
2016-09-16 10:49:08 +0100

    changes labels for nodes and connections from 1-5 to 1-3

--- src/entity/edges.js
+++ src/entity/edges.js
@@ -16,10 +16,10 @@ module.exports = (nga, admin) => {
       .label('Strength of Relationship')
       .choices([
         { value: '1', label: '1' },
-        { value: '1.5', label: '2' },
-        { value: '2', label: '3' },
-        { value: '2.5', label: '4' },
-        { value: '3', label: '5' },
+        { value: '1.5', label: '1.5' },
+        { value: '2', label: '2' },
+        { value: '2.5', label: '2.5' },
+        { value: '3', label: '3' },
       ])
       .defaultValue(2),
     nga.field('clientId')
